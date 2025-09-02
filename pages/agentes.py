5246b0dc5b51ea8384a4c8b0e740b1138390fb3f
Brenda <brentorres26@gmail.com>
2025-09-02 18:53:43 -0300

    Creaciñon de páginas

---
+++ pages/agentes.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+#------------------------------Dashboard
+st.set_page_config(
+    page_title="Call center",
+    page_icon="📞",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("📈 Call center")
